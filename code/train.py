a819fe4bbb642ed0a1e1069e4a3758f062e3ea2e
Yan <astromid@users.noreply.github.com>
2018-01-09 01:04:36 +0300

    multiprocessing test

--- code/train.py
+++ code/train.py
@@ -66,7 +66,8 @@ hist = model.fit_generator(
     validation_data=val_seq,
     validation_steps=len(val_seq),
     max_queue_size=20,
-    workers=2
+    workers=2,
+    use_multiprocessing=True
 )
 model.save(os.path.join(MODEL_DIR, 'model.h5'))
 print('Model saved successfully')
