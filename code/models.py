f1bca99a0f3df592f13f7afc14f6c6b3a37180fd
Yan <astromid@users.noreply.github.com>
2018-01-10 23:47:25 +0300

    many bugs fixed

--- code/models.py
+++ code/models.py
@@ -1,5 +1,7 @@
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Conv2D, Conv1D, Dense, Input, Flatten
+from tensorflow.python.keras.layers import Conv2D, Conv1D, Dense
+from tensorflow.python.keras.layers import Activation, Input, Flatten
+from tensorflow.python.keras.layers import Multiply, Add
 from tensorflow.python.keras.layers import MaxPooling2D, AveragePooling2D
 from tensorflow.python.keras.layers import GlobalAveragePooling2D
 from tensorflow.python.keras.layers import Dropout, BatchNormalization
@@ -51,14 +53,14 @@ class SeResNet3:
 
         conv1 = Conv2D(filters=16, kernel_size=(3, 3), padding='same')(norm_i)
         bn1 = BatchNormalization()(conv1)
-        relu1 = relu(bn1)
+        relu1 = Activation(activation='relu')(bn1)
         res1 = self.resblock(z=relu1, n_in=16, n_out=16)
         pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(res1)
         drp1 = Dropout(rate=0.1)(pool1)
 
         conv2 = Conv2D(filters=32, kernel_size=(3, 3), padding='same')(drp1)
         bn2 = BatchNormalization()(conv2)
-        relu2 = relu(bn2)
+        relu2 = Activation(activation='relu')(bn2)
         res2 = self.resblock(z=relu2, n_in=32, n_out=32)
         res3 = self.resblock(z=res2, n_in=32, n_out=32)
         pool2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(res3)
@@ -66,7 +68,7 @@ class SeResNet3:
 
         conv3 = Conv2D(filters=64, kernel_size=(3, 3), padding='same')(drp2)
         bn3 = BatchNormalization()(conv3)
-        relu3 = relu(bn3)
+        relu3 = Activation(activation='relu')(bn3)
         res4 = self.resblock(z=relu3, n_in=64, n_out=64)
         res5 = self.resblock(z=res4, n_in=64, n_out=64)
         pool3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(res5)
@@ -74,15 +76,15 @@ class SeResNet3:
 
         conv4 = Conv2D(filters=128, kernel_size=(3, 3), padding='same')(drp3)
         bn4 = BatchNormalization()(conv4)
-        relu4 = relu(bn4)
+        relu4 = Activation(activation='relu')(bn4)
         res6 = self.resblock(z=relu4, n_in=128, n_out=128)
         res7 = self.resblock(z=res6, n_in=128, n_out=128)
         drp4 = Dropout(rate=0.2)(res7)
 
         conv5 = Conv2D(filters=256, kernel_size=(3, 3), padding='same')(drp4)
         bn5 = BatchNormalization()(conv5)
-        relu5 = relu(bn5)
-        pool5 = AveragePooling2D(pool_size=(2, 2))(relu5)
+        relu5 = Activation(activation='relu')(bn5)
+        pool5 = GlobalAveragePooling2D()(relu5)
 
         flat1 = Flatten()(pool5)
         dense1 = Dense(units=256, activation=relu)(flat1)
@@ -99,17 +101,20 @@ class SeResNet3:
         self.model = model
 
     def scale(self, z, n, red=16):
-        pool1 = GlobalAveragePooling2D()(z)
-        conv1 = Conv1D(filters=red, kernel_size=1, activation=relu)(pool1)
-        conv2 = Conv1D(filters=n, kernel_size=1)(conv1)
-        return sigmoid(conv2)
+        # pool1 = AveragePooling2D(pool_size=z.shape[2:])(z)
+        # pool1.reshape(pool1.shape + (1,))
+        conv1 = Conv2D(filters=red, kernel_size=(1, 1), activation=relu)(z)
+        conv2 = Conv2D(filters=n, kernel_size=(1, 1))(conv1)
+        return Activation(activation='sigmoid')(conv2)
 
     def resblock(self, z, n_in, n_out):
         conv1 = Conv2D(n_in, (3, 3), padding='same')(z)
         bn1 = BatchNormalization()(conv1)
-        relu1 = relu(bn1)
+        relu1 = Activation(activation='relu')(bn1)
         conv2 = Conv2D(n_out, (3, 3), padding='same')(relu1)
         bn2 = BatchNormalization()(conv2)
         scale1 = self.scale(bn2, n_out)
-        out = z + scale1 * bn2
-        return relu(out)
+        mul1 = Multiply()([scale1, bn2])
+        add1 = Add()([z, mul1])
+        out = Activation(activation='relu')(add1)
+        return out
